0ba39cc2200335de070ae4523d15d4fbaf236009
Liuda <lrezunik@edu.hse.ru>
2022-04-18 13:19:08 +0300

    add Seminar 2 template

---
+++ Seminar 2/Seminar 2/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Seminar 2
+//
+//  Created by Ludmila Rezunic on 18.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .purple
+        // Do any additional setup after loading the view.
+    }
+    
+    
+
+
+}
+
