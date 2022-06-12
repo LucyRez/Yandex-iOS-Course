f4d4aa9e4960b9760d9b8c9eafa598510ba1270d
Liuda <lrezunik@edu.hse.ru>
2022-06-12 17:46:09 +0300

    add data models

---
+++ RickAndMortyApp/RickAndMortyApp/Models/APIResponseModel.swift
@@ -0,0 +1,20 @@
+//
+//  APIResponseModel.swift
+//  RickAndMortyApp
+//
+//  Created by Ludmila Rezunic on 12.06.2022.
+//
+
+import Foundation
+
+struct APIResponseModel: Codable{
+    let info: InfoModel
+    let results: [Character]
+}
+
+struct InfoModel: Codable{
+    let count: Int
+    let pages: Int
+    let next: String
+    
+}
--- RickAndMortyApp/RickAndMortyApp/Models/Character.swift
+++ RickAndMortyApp/RickAndMortyApp/Models/Character.swift
@@ -6,7 +6,22 @@
 //
 
 import Foundation
+import UIKit
 
-struct Character{
+struct Character: Codable{
+    let id: Int
+    let name: String
+    let status: String
+    let species: String
+    let gender: String
+    let imageURL: String
     
+    enum CodingKeys: String, CodingKey{
+        case id, name, status, species, gender
+        case imageURL = "image"
+    }
 }
+
+    
+    
+   
